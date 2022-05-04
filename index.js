c64a4a08bc852f65cf122ad06c6d670e06208547
Nickothan <s1nicolasrg@gmail.com>
2022-05-04 11:50:32 -0500

    Creando ruta 404.

--- index.js
+++ index.js
@@ -8,41 +8,44 @@ const app = http.createServer((request, response) => {
   response.end(JSON.stringify(notes))
 }) */
 
-const { request, response } = require("express")
-const express = require("express")
-const app = express()
+const { request, response } = require("express");
+const express = require("express");
+const app = express();
+const logger = require("./loggerMiddleware");
 
-app.use(express.json())
+app.use(express.json());
+
+app.use(logger);
 
 let notes = [
-  {
-    "id": 1,
-    "content": "Me tengo que suscribir amidudeb en youtube.",
-    "date": "2019-05-30T17:30:31.098Z",
-    "importnant": true
-  },
-  {
-    "id": 2,
-    "content": "Tengo que estudiar para ser full stack.",
-    "date": "2019-05-30T18:30:31.098Z",
-    "importnant": false
-  },
-  {
-    "id": 3,
-    "content": "Resolver retos.",
-    "date": "2019-05-30T19:30:31.098Z",
-    "importnant": true
-  },
-]
+	{
+		id: 1,
+		content: "Me tengo que suscribir amidudeb en youtube.",
+		date: "2019-05-30T17:30:31.098Z",
+		importnant: true,
+	},
+	{
+		id: 2,
+		content: "Tengo que estudiar para ser full stack.",
+		date: "2019-05-30T18:30:31.098Z",
+		importnant: false,
+	},
+	{
+		id: 3,
+		content: "Resolver retos.",
+		date: "2019-05-30T19:30:31.098Z",
+		importnant: true,
+	},
+];
 
 // Peticion get
-app.get('/', (request, response) => {
-  response.send('<h1>Hello word</h1>')
-})
+app.get("/", (request, response) => {
+	response.send("<h1>Hello word</h1>");
+});
 
-app.get('/api/notes', (request, response) =>{
-  response.json(notes)
-})
+app.get("/api/notes", (request, response) => {
+	response.json(notes);
+});
 /*  maneras de usar app
 .get
 .post
@@ -50,57 +53,63 @@ app.get('/api/notes', (request, response) =>{
 .put */
 
 // Recuperar objeto por su id
-app.get('/api/notes/:id', (request, response) =>{
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-  
-  if(note){
-    response.json(note)
-  }else {
-    response.status(404).end()
-  }
-})
+app.get("/api/notes/:id", (request, response) => {
+	const id = Number(request.params.id);
+	const note = notes.find((note) => note.id === id);
+
+	if (note) {
+		response.json(note);
+	} else {
+		response.status(404).end();
+	}
+});
 
 //Eliminar objeto por su id
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id === id)
-  // Estado de nada en el array
-  response.status(204).end()
-})
+app.delete("/api/notes/:id", (request, response) => {
+	const id = Number(request.params.id);
+	notes = notes.filter((note) => note.id === id);
+	// Estado de nada en el array
+	response.status(204).end();
+});
 
 // Crear nota
 app.post("/api/notes", (request, response) => {
-  const note = request.body
+	const note = request.body;
 
-  if(!note || !note.content){
-    return response.status(404).json({
-      error: 'note.content is missing'
-    })
-  }
+	if (!note || !note.content) {
+		return response.status(404).json({
+			error: "note.content is missing",
+		});
+	}
 
-  //buscar id mas alto
-  const ids = notes.map(note => note.id)
-  const maxId = Math.max(...ids)
+	//buscar id mas alto
+	const ids = notes.map((note) => note.id);
+	const maxId = Math.max(...ids);
 
-  // Creando el objeto
-  const newNote = {
-    id: maxId + 1,
-    content : note.content,
-    importnant : typeof note.importnant === "undefined" ? note.importnant : false,
-    date: new Date().toISOString()
-  }
+	// Creando el objeto
+	const newNote = {
+		id: maxId + 1,
+		content: note.content,
+		importnant:
+			typeof note.importnant === "undefined" ? note.importnant : false,
+		date: new Date().toISOString(),
+	};
 
-  //Agregando el objeto en la ultima posicion
-  //notes = notes.concat(newNote)
-  notes = [...notes, newNote]
+	//Agregando el objeto en la ultima posicion
+	//notes = notes.concat(newNote)
+	notes = [...notes, newNote];
 
-  response.status(201).json(newNote)
-})
+	response.status(201).json(newNote);
+});
 
+app.use((request, response) => {
+	response.status(404).json({
+		error: "Not found.",
+	});
+});
 
 //iniciando servidor en el puerto 3001
-const PORT = 3001
+const PORT = 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port: ${PORT}`)
-})
+	console.log(`Server running on port: ${PORT}`);
+});
